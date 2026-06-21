5dbbea1c87a163282884016a48df762c2bba71f0
Aditya Anand <contactaditechdev@gmail.com>
2026-06-21 12:38:53 +0530

    Minor change

--- src/accessmodifier/Client.java
+++ src/accessmodifier/Client.java
@@ -1,5 +1,5 @@
 package accessmodifier;
-
+// https://stackoverflow.com/questions/215497/what-is-the-difference-between-public-protected-package-private-and-private-in
 public class Client {
   public static void main(String[] args) {
       Student demo = new Student();
